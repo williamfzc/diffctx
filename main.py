8da62bd6fa9c5726927c0ed2895ec38a721c6022
williamfzc <178894043@qq.com>
2023-04-15 00:27:21 +0800

    feat: before and after sha

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ support_langs = {"golang", "python"}
 
 
 def gen_index(lang: str):
-    if lang not in support_langs:
-        logger.error(f"lang not supported: {lang}, supported: {support_langs}")
-        return
-
     files = os.listdir(user_dir)
     logger.info(f"files: {files}")
 
@@ -22,7 +18,6 @@ def gen_index(lang: str):
     else:
         logger.error("no index mapping")
         return
-    _check_call(["srctx", "diff"])
 
 
 def gen_golang_index():
@@ -37,11 +32,29 @@ def _check_call(commands: list):
     subprocess.check_call(commands, cwd=user_dir)
 
 
+def set_safe_git_dir():
+    _check_call(["git", "config", "--global", "--add", "safe.directory", user_dir])
+
+
 def main():
     args = sys.argv[1:]
     logger.info(f"args: {args}")
     lang = args[0]
+    before_sha = args[1]
+    after_sha = args[2]
+    # check
+    if lang not in support_langs:
+        logger.error(f"lang not supported: {lang}, supported: {support_langs}")
+        return
+    if not before_sha or not after_sha:
+        logger.warning("sha empty. Use default.")
+        before_sha = "HEAD~1"
+        after_sha = "HEAD"
+
     gen_index(lang)
+    # gen diff
+    set_safe_git_dir()
+    _check_call(["srctx", "diff", "--src", user_dir, "--before", before_sha, "--after", after_sha])
 
 
 if __name__ == "__main__":
