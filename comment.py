4ccc07ffe2de90a4ceff174a1e62e093534ed800
williamfzc <178894043@qq.com>
2023-04-19 00:24:09 +0800

    fix: wrong comment api

--- comment.py
+++ comment.py
@@ -27,11 +27,12 @@ def send_code_comments(token: str, repo_id: str, issue_number: int, lines: typin
     repo = g.get_repo(repo_id)
     pr = repo.get_pull(issue_number)
 
-    commit = pr.get_commits()[0]
+    commit = pr.get_commits()[-1]
+    logger.info(f"commits: {commit}")
     for each_line in lines:
         if each_line.refScope.crossFileRefCount > 0:
-            logger.info(f"leave comment in {each_line.fileName} #{each_line.lineNumber}")
-            pr.create_comment(
+            logger.info(f"leave comment in {each_line.fileName} L{each_line.lineNumber}")
+            pr.create_review_comment(
                 f"[diffctx] cross file reference: {each_line.refScope.crossFileRefCount}",
                 commit,
                 each_line.fileName,
