89421a9255f18a916dd29ed3c3438024dcda0bb1
williamfzc <178894043@qq.com>
2023-05-10 22:54:57 +0800

    chore: test project for java and kotlin

--- debug.py
+++ debug.py
@@ -9,19 +9,19 @@ import shutil
 def debug_main():
     # pull some repos for test
     check_call(["git", "clone", "--depth=2", "https://github.com/gin-gonic/gin.git"])
-    check_call(["git", "clone", "--depth=2", "https://github.com/google/guava.git"])
+    # check_call(["git", "clone", "--depth=2", "https://github.com/square/okhttp"])
 
     gen_index("golang", "gin")
-    gen_index("java", "guava")
+    # gen_index("kotlin", "okhttp")
 
     os.chdir("gin")
     gen_diff("HEAD~1", "HEAD", "")
     os.chdir("..")
 
-    os.chdir("guava")
-    gen_diff("HEAD~1", "HEAD", "")
-    os.chdir("..")
+    # os.chdir("okhttp")
+    # gen_diff("HEAD~1", "HEAD", "")
+    # os.chdir("..")
 
     # clean up
     shutil.rmtree("gin")
-    shutil.rmtree("guava")
+    # shutil.rmtree("okhttp")
