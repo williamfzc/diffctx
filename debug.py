a8203f568128c152c4b39e4220ebe729577006ac
williamfzc <178894043@qq.com>
2023-05-20 15:39:04 +0800

    feat: java test

--- debug.py
+++ debug.py
@@ -9,19 +9,19 @@ import shutil
 def debug_main():
     # pull some repos for test
     check_call(["git", "clone", "--depth=2", "https://github.com/gin-gonic/gin.git"])
-    # check_call(["git", "clone", "--depth=2", "https://github.com/square/okhttp"])
+    check_call(["git", "clone", "--depth=2", "https://github.com/google/guava"])
 
     gen_index("golang", "gin", "")
-    # gen_index("kotlin", "okhttp")
+    gen_index("java", "guava", "")
 
     os.chdir("gin")
     gen_diff("HEAD~1", "HEAD", "")
     os.chdir("..")
 
-    # os.chdir("okhttp")
-    # gen_diff("HEAD~1", "HEAD", "")
-    # os.chdir("..")
+    os.chdir("guava")
+    gen_diff("HEAD~1", "HEAD", "")
+    os.chdir("..")
 
     # clean up
     shutil.rmtree("gin")
-    # shutil.rmtree("okhttp")
+    shutil.rmtree("guava")
