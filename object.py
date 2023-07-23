8779b691bd7e388c2a9afb088e1233f3d8945114
williamfzc <178894043@qq.com>
2023-07-23 20:15:37 +0800

    fix: default value

--- object.py
+++ object.py
@@ -24,8 +24,8 @@ class MetricsResponse(BaseModel):
 
 class StatGlobal(BaseModel):
     unitLevel: str
-    unitMapping: typing.Dict[str, int]
-    impactUnits: typing.List[int]
-    transImpactUnits: typing.List[int]
-    entries: typing.List[int]
-    impactEntries: typing.List[int]
+    unitMapping: typing.Dict[str, int] = dict()
+    impactUnits: typing.List[int] = []
+    transImpactUnits: typing.List[int] = []
+    entries: typing.List[int] = []
+    impactEntries: typing.List[int] = []
