6922b8c3ff97470aded7805da633723267a567e9
williamfzc <178894043@qq.com>
2023-07-26 22:37:52 +0800

    fix: add `node`

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USER_DIR = "/github/workspace"
-SUPPORT_LANGS = {"golang", "python", "java", "kotlin"}
+SUPPORT_LANGS = {"golang", "python", "java", "kotlin", "node"}
 CSV_RESULT_FILE = "./output.csv"
 JSON_RESULT_FILE = "./output.json"
 STAT_JSON_RESULT_FILE = "./stat.json"
